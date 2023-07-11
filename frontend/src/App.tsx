24a62f6c1729b30b8ea674a60d5f1ffac94e66ff
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-07-11 09:13:44 +0700

    feat(config): set up language

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,29 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/home'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { Suspense } from 'react'
+// import { useTranslation } from 'react-i18next'
 
 const client = new QueryClient()
 
 function App() {
+  // const { i18n, t } = useTranslation();
+
+  // const onChangeLang = (lang_code: "vn" | "en") => {
+  //   i18n.changeLanguage(lang_code);
+  // };
 
   return (
-    <QueryClientProvider client={client}>
-
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Homepage/>} />
-      </Routes>
-    </BrowserRouter>
-    </QueryClientProvider>
+    <Suspense fallback="loading">
+      <QueryClientProvider client={client}>
+        {/* <h1 onClick={() => onChangeLang("vn")}>{t("home")}</h1> */}
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Suspense >
   )
 }
 
---
+++ frontend/src/pages/home/components/BoxChat.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const BoxChat = (props: Props) => {
+  return (
+    <div>BoxChat</div>
+  )
+}
+
+export default BoxChat
