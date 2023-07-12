73bb7678ae1c66c587f613ecb810a6da8d6b3677
Mai Hữu Nghĩa <emches1976@gmail.com>
2023-07-12 11:35:39 +0700

    [PBI4] avatar + icon dynamic

--- frontend/src/pages/ranking-board/components/UserFrame.tsx
+++ frontend/src/pages/ranking-board/components/UserFrame.tsx
@@ -1,62 +1,74 @@
 import { Dialog, DialogTrigger } from '@/common/components/ui/Modal';
-import { Check, Home, LucideIcon } from 'lucide-react';
-import React from 'react';
+import { Home, LucideIcon } from 'lucide-react';
+import React, { useRef, useState } from 'react';
 import { DialogDemo } from './modals/ProfileModal';
+import { cn } from '@/common/lib/utils';
+import { Avatar, AvatarFallback, AvatarImage } from '@/common/components/ui/avatar';
 
 interface ProfileProps {
     Leaderboard: Array<{
         img: string;
         name: string;
         score: number;
+        host: boolean;
+        type?: string;
         icon?: LucideIcon;
     }>;
 }
 
 const UserFrame: React.FC<ProfileProps> = ({ Leaderboard }) => {
+
+    const [userSelected, setUserSelected] = useState(null)
+    const triggerRef = useRef<HTMLButtonElement | null>(null)
+    const handleLinkClick = (value: any) => {
+        setUserSelected(value)
+        triggerRef.current?.click()
+    };
     const renderItem = (data: typeof Leaderboard) => (
         <>
             {data.map((value, _index) => (
-                <Dialog>
-                    <DialogTrigger asChild>
-                        <a href="#" className="group block">
-                            <li className="py-3 sm:py-4">
-                                <div className="flex items-center space-x-4">
-                                    <Check size={36} color="#22A62F" strokeWidth={4} />
-                                    <div className="flex-shrink-0">
-                                        <img className="w-20 h-20 rounded-full group-hover:scale-110" src={value.img} alt="avatar" />
-                                    </div>
-                                    <div className="flex-1 min-w-0">
-                                        <p className="text-lg font-medium text- truncate dark:text-white">
-                                            {value.name}
-                                        </p>
-                                        <p className="text-sm text-textBlueColor truncate dark:text-gray-400">
-                                            {value.score}
-                                        </p>
-                                    </div>
-                                    <Home color="#2062fb" strokeWidth={2.5} />
+                <a href="#" className="group block" onClick={() => handleLinkClick(value)}>
+                    <li className="py-3 sm:py-4 flex">
+                        <div className="flex items-center space-x-3">        
+                            <div className={cn('flex items-center space-x-4 w-[40px]', value.type)}>
+                                {value.icon && <value.icon size={36} strokeWidth={3.5} />}
+                            </div>
+                            <div className={cn('flex items-center space-x-4', value.type)}>
+                                <Avatar className="flex items-center bg-yellow-300 w-1/5 h-auto rounded-full group-hover:scale-110">
+                                    <AvatarImage src={value?.img} alt="avatar" />
+                                    <AvatarFallback>Avatar</AvatarFallback>
+                                </Avatar>
+                                <div className="flex-1 min-w-0">
+                                    <p className="text-lg font-medium text-truncate dark:text-white">
+                                        {value.name}
+                                    </p>
+                                    <p className="text-sm text-textBlueColor truncate dark:text-gray-400">
+                                        {value.score}
+                                    </p>
                                 </div>
-                            </li>
-                        </a>
-                    </DialogTrigger>
-                    <DialogDemo />
-                </Dialog>
+                                {value.host && <Home color="#2062fb" strokeWidth={2.5} />}
+                            </div>
+                        </div>
+                    </li>
+                </a>
+
             ))}
         </>
     );
 
     return (
         <div id="profile" className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 bg-center self-center">
-            <div className="flex items-center justify-between mb-4">
-                <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white flex items-center justify-center">Leaderboard</h5>
-
-            </div>
-
             <div className="flow-root">
                 <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
                     {renderItem(Leaderboard)}
                 </ul>
             </div>
-
+            { }
+            <Dialog>
+                <DialogTrigger ref={triggerRef}>
+                </DialogTrigger>
+                <DialogDemo user={userSelected} />
+            </Dialog>
         </div>
     );
 };
--- frontend/src/pages/ranking-board/components/modals/ProfileModal.tsx
+++ frontend/src/pages/ranking-board/components/modals/ProfileModal.tsx
@@ -1,39 +1,50 @@
 import { Button } from "@/common/components/ui/Button"
 import {
-    Dialog,
     DialogContent,
-    DialogDescription,
     DialogFooter,
     DialogHeader,
-    DialogTitle,
-    DialogTrigger,
 } from "@/common/components/ui/Modal"
-import { Input } from "@/common/components/ui/Input"
 import { Label } from "@/common/components/ui/label"
+import { Ban, ThumbsDown } from "lucide-react"
+import { Avatar, AvatarFallback, AvatarImage } from "@/common/components/ui/avatar"
 
-export function DialogDemo() {
+type Props = {
+    user: any
+}
+
+export function DialogDemo({ user }: Props) {
+
     return (
-        <DialogContent className="sm:max-w-[425px]">
+        <DialogContent className="sm:w-[425px]">
             <DialogHeader>
-                <DialogTitle>Profile</DialogTitle>
-            </DialogHeader>
-            <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                        Name
-                    </Label>
-                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                <div className="flex items-center justify-center gap-5 w-full">
+                    <img className="w-4/5 h-fit" src="src/common/assets/ProfileLabel.png" alt="avatar" />
                 </div>
-                <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="username" className="text-right">
-                        Username
+            </DialogHeader>
+            <div className="flex flex-col items-center justify-center gap-5 w-full py-5">
+                {/* <div className="flex items-center justify-center w-full">
+                    <img className="w-40 h-40 rounded-full" src={user?.img} alt="avatar" />
+                </div> */}
+                <Avatar className="flex items-center bg-yellow-300 w-1/3 h-auto rounded-full">
+                    <AvatarImage src={user?.img} alt="avatar" />
+                    <AvatarFallback>Avatar</AvatarFallback>
+                </Avatar>
+
+                <div className="flex items-center justify-center w-full">
+                    <Label htmlFor="name" className="text-lg font-medium text-truncate dark:text-white">
+                        {user?.name}
                     </Label>
-                    <Input id="username" value="@peduarte" className="col-span-3" />
                 </div>
             </div>
             <DialogFooter>
-                <Button type="submit" className="flex items-center justify-center">Save changes</Button>
-
+                <div className="flex flex-col items-center justify-center gap-5 w-full">
+                    <div className="flex items-center justify-center w-full">
+                        <Button className="rounded-l-full rounded-r-full ring-8 ring-black bg-yellow-600 hover:bg-yellow-200 text-white"><ThumbsDown size={24} color="#000" strokeWidth={4} className="mr-1" />VOTE KICK</Button>
+                    </div>
+                    <div className="flex items-center justify-center w-full">
+                        <Button variant="destructive" className="rounded-l-full rounded-r-full ring-8 ring-black bg-red-700 hover:bg-red-600 text-white"><Ban size={24} color="#000" strokeWidth={4} className="mr-5" />BLOCKS</Button>
+                    </div>
+                </div>
             </DialogFooter>
         </DialogContent>
     )
