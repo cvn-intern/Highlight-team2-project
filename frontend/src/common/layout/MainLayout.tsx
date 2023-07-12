3b2661c9bb6e28067694d2af27dcefb94b39afdf
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-07-12 15:46:28 +0700

    PBI2: fix layout

--- frontend/src/common/layout/MainLayout.tsx
+++ frontend/src/common/layout/MainLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div id="main-layout" className='w-screen h-screen bg-blue-500 flex flex-col items-center justify-center'>
+    <div id="main-layout" className='w-screen h-screen bg-blue-500 '>
         {children}
     </div>
   )
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -27,15 +27,17 @@ const Homepage = () => {
 
   return (
     <MainLayout>
-      <Logo />
-      <img src={SloganImg} alt="" className='w-[300px] mt-5' />
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <Logo />
+        <img src={SloganImg} alt="" className='w-[300px] mt-5' />
 
-      <div className='w-[80%] h-[70%] bg-white flex flex-col items-center mt-5'>
-        <img src={BannerImg} className='my-5' />
+        <div className='w-[80%] h-[70%] bg-white flex flex-col items-center mt-5'>
+          <img src={BannerImg} className='my-5' />
 
-        <div className='flex items-center gap-24'>
-          <CustomAvatar />
-          <PlayForm />
+          <div className='flex items-center gap-24'>
+            <CustomAvatar />
+            <PlayForm />
+          </div>
         </div>
       </div>
     </MainLayout>
