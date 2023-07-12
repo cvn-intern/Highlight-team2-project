42dde56b237ccc5c1926759e378e95eea4da4de7
trungnguyenhuynhminh46 <58035150+trungnguyenhuynhminh46@users.noreply.github.com>
2023-07-12 10:30:16 +0700

    .

--- frontend/src/common/stores/paintStore.ts
+++
@@ -1,9 +0,0 @@
-import { create, StateCreator } from "zustand";
-
-interface PaintSlice {}
-
-const createPaintSlice: StateCreator<PaintSlice> = (set) => ({
-  // States
-});
-
-export const useExampleStore = create<PaintSlice>();
