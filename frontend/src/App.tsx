3287e1945c09a5d47d4a99af96149ba538d2f83d
Vân Khánh <92099362+hnvkhanh@users.noreply.github.com>
2023-07-14 14:15:17 +0700

    refactor

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
 
   useEffect(() => {
     const createSocketInstance = (token: string) =>  {
-      const socketInit = io("http://localhost:3001", {
+      const socketInit = io("https://f6fc-118-69-74-9.ngrok-free.app/", {
         extraHeaders: {
           authorization: token,
         },
--- frontend/src/common/lib/config.ts
+++ frontend/src/common/lib/config.ts
@@ -1,1 +1,1 @@
-export const baseURL = "http://localhost:3000/api/v1/"
+export const baseURL = "https://f6fc-118-69-74-9.ngrok-free.app/api/v1"
