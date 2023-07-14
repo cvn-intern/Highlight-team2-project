b8dc5bcafb08365228761c652d5940ee080cac6b
Vân Khánh <92099362+hnvkhanh@users.noreply.github.com>
2023-07-14 14:35:28 +0700

    Update i18n.ts

--- frontend/src/common/lib/i18n.ts
+++ frontend/src/common/lib/i18n.ts
@@ -5,7 +5,7 @@ import { initReactI18next } from "react-i18next";
 const getCurrentHost =
   import.meta.env.MODE === "development"
     ? "http://localhost:5173"
-    : "LINK TO PROD";
+    : "https://highlight-io.netlify.app";
 
 i18n.use(i18nBackend).use(initReactI18next).init({
     lng: "en",
