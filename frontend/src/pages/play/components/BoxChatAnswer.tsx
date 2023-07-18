0f7546f5f0ca22fc6a4ac121e7b06c18c8948d3f
hnvkhanh <hnvk101@gmail.com>
2023-07-18 17:35:56 +0700

    PBI9: display answer on answer box

--- frontend/src/pages/play/components/BoxChatAnswer.tsx
+++ frontend/src/pages/play/components/BoxChatAnswer.tsx
@@ -63,6 +63,11 @@ const BoxChat = (props: BoxProps) => {
             codeRoom: codeRoom,
             message: inputChat,
           } as MessageBodyInterface)
+        }else{         
+          socket?.emit('answer-room', {
+            codeRoom: codeRoom,
+            message: inputChat,
+          } as MessageBodyInterface)
         }
 
         SetInputChat("")
@@ -115,6 +120,11 @@ const BoxChatAnswer = ({ }: Props) => {
       setListChat(pre => [...pre, data])
     })
 
+    socket?.on(`${codeRoom}-answer`, (data: Chat) => {
+      console.log(data);
+      setListAnswer(pre => [...pre, data])
+    })
+
     socket?.on(`${codeRoom}-leave`, (data: Chat) => {
       console.log(data);
       setListChat(pre => [...pre, data])
@@ -123,6 +133,7 @@ const BoxChatAnswer = ({ }: Props) => {
     return () => {
       socket?.off(codeRoom)
       socket?.off(`${codeRoom}-chat`)
+      socket?.off(`${codeRoom}-answer`)
       socket?.off(`${codeRoom}-leave`)
     }
   }, [socket])
--- frontend/src/pages/play/constants/icons.ts
+++ frontend/src/pages/play/constants/icons.ts
@@ -1,7 +1,8 @@
-import { Edit2, Info, LogOut, MessageCircle } from "lucide-react";
+import { Edit2, Info, LogOut, MessageCircle, BadgeCheck } from "lucide-react";
 
 export const iconsMap = new Map();
 iconsMap.set("Edit2", Edit2);
 iconsMap.set("Info", Info);
 iconsMap.set("MessageCircle", MessageCircle);
 iconsMap.set("LogOut", LogOut); 
+iconsMap.set("BadgeCheck", BadgeCheck); 
