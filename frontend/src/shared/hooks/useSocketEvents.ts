b6ee32f509552133d26dffdd12479b1c822ea392
Mai Hữu Nghĩa <emches1976@gmail.com>
2023-07-19 11:45:07 +0700

    Update useSocketEvents.ts

--- frontend/src/shared/hooks/useSocketEvents.ts
+++ frontend/src/shared/hooks/useSocketEvents.ts
@@ -1,10 +1,10 @@
 import { useEffect } from "react";
 import { useSocketStore } from "@/shared/stores/socketStore";
 
-interface Event{
-    name: string,
-    handler(...args: any[]): any;
-}
+// interface Event{
+//     name: string,
+//     handler(...args: any[]): any;
+// }
 
 export const useSocketEvents = (eventName: string, eventHandler: any) => {
     const {socket} = useSocketStore()
