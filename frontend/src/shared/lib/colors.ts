a8c364ff690710bb02dbd5778da0e3aa2fd5c9d7
trungnguyenhuynhminh46 <trungnguyenhuynhminh46@gmail.com>
2023-07-22 19:43:32 +0700

    Resolve review source: Refactor lib/colors

--- frontend/src/shared/lib/colors.ts
+++ frontend/src/shared/lib/colors.ts
@@ -1,50 +1,41 @@
-export const rgbaToHex = (r: number, g: number, b: number, a: number) => {
-  var r_hex = r.toString(16);
-  var g_hex = g.toString(16);
-  var b_hex = b.toString(16);
-  var a_hex = Math.round(a * 255).toString(16);
-
-  if (r_hex.length == 1) {
-    r_hex = "0" + r_hex;
-  }
-  if (g_hex.length == 1) {
-    g_hex = "0" + g_hex;
-  }
-  if (b_hex.length == 1) {
-    b_hex = "0" + b_hex;
-  }
-  if (a_hex.length == 1) {
-    a_hex = "0" + a_hex;
-  }
-
-  return "#" + r_hex + g_hex + b_hex + a_hex;
-};
-
-export const rgbStringToObject = (rgbString: string) => {
-  const [r, g, b] = rgbString.replace(/[^\d,]/g, "").split(",");
-  const rgbObject = {
-    r: parseInt(r),
-    g: parseInt(g),
-    b: parseInt(b),
-    a: 1,
+// Convert color from rbga standard to hex standard
+export const rgbaToHex = (
+  red: number,
+  green: number,
+  blue: number,
+  alpha: number
+): string => {
+  const MAX_ALPHA_IN_255_SCALE = 255;
+  const toHex = (value: number, isAlpha = false) => {
+    const hex = isAlpha
+      ? Math.round(value * MAX_ALPHA_IN_255_SCALE).toString(16)
+      : value.toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
   };
-  return rgbObject;
+
+  return "#" + toHex(red) + toHex(green) + toHex(blue) + toHex(alpha, true);
 };
 
+// Convert rgb string (Ex: 'rgba(255, 255, 0, 1)') to rgba object (Ex: {r: 255, g: 255, b: 0, a: 1})
+export const rgbStringToObject = (rgbString: string) => {
+  const DEFAULT_ALPHA = 1;
+  const [r, g, b] = rgbString
+    .replace(/[^\d,]/g, "")
+    .split(",")
+    .map(Number);
+  return { r, g, b, a: DEFAULT_ALPHA };
+};
+
+// Convert hex string (Ex: '#ffff00') to rgba object (Ex: {r: 255, g: 255, b: 0, a: 1})
 export const hexToRGBA = (
   hexCode: string
 ): { r: number; g: number; b: number; a: number } => {
-  hexCode = hexCode.replace("#", "");
+  const DEFAULT_ALPHA = 1;
+  const hexWithoutHash = hexCode.replace("#", "");
+  const r = parseInt(hexWithoutHash.substring(0, 2), 16);
+  const g = parseInt(hexWithoutHash.substring(2, 4), 16);
+  const b = parseInt(hexWithoutHash.substring(4, 6), 16);
+  const a = DEFAULT_ALPHA;
 
-  const r = parseInt(hexCode.substring(0, 2), 16);
-  const g = parseInt(hexCode.substring(2, 4), 16);
-  const b = parseInt(hexCode.substring(4, 6), 16);
-  const a = 1;
-
-  return {
-    r,
-    g,
-    b,
-    a,
-  };
+  return { r, g, b, a };
 };
