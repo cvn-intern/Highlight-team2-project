36f83ce7fe5aed08c771d1c57418d41eeb510f54
trungnguyenhuynhminh46 <trungnguyenhuynhminh46@gmail.com>
2023-07-26 20:23:13 +0700

    Fix merge error

--- frontend/src/applications/play/Page.tsx
+++ frontend/src/applications/play/Page.tsx
@@ -1,16 +1,17 @@
-import { useEffect, useMemo } from "react";
+import { useEffect } from "react";
 import PlayingGameScreen from "./PlayingGameScreen.component";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import WaitingRoom from "../waiting-room/Page";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useSocketStore } from "@/shared/stores/socketStore";
 
 export default function Page() {
   const { state } = useLocation();
   const { socket } = useSocketStore();
   const navigate = useNavigate();
-  const { codeRoom } = useParams();
 
   useEffect(() => {
     socket?.on("error", () => {
+      navigate("/user/existing");
     });
 
     return () => {
@@ -19,11 +20,7 @@ export default function Page() {
   }, [socket]);
 
   // If 'wait' is undefinded, 'isRenderWaiting' is still be true
-  const isRenderWaiting = useMemo(() => state === null ? true : !!state.wait, [state])
-
-  useEffect(() => {
-    if(isRenderWaiting) navigate("../" + codeRoom + "/waiting", {replace: true})
-  }, [isRenderWaiting, codeRoom])
-
-  if(!isRenderWaiting) return <PlayingGameScreen />;
+  const isRenderWaiting = state === null ? true : !!state.wait;
+  if (isRenderWaiting) return <WaitingRoom />;
+  return <PlayingGameScreen />;
 }
