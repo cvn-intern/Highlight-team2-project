c50e60df0e6ceec2b9755978419e4df2ab9403b0
hnvkhanh <hnvk101@gmail.com>
2023-07-27 14:37:47 +0700

    PBI18: revert import

--- frontend/src/applications/play/PlayingGameScreen.component.tsx
+++ frontend/src/applications/play/PlayingGameScreen.component.tsx
@@ -1,30 +1,30 @@
+import { useRef, useState, useEffect, createContext } from "react";
 import useDisableBackButton from "@/shared/hooks/useDisableBackButton";
-import { createContext, useEffect, useRef, useState } from "react";
 // Variables
 import { DEFAULT_BLACK } from "./shared/constants/color";
 // Components
 import MainLayout from "@/shared/components/MainLayout";
-import BoxChatAnswer from "./chat-answer/BoxChatAnswer.component";
-import Canvas from "./draw-screen/Canvas.component";
-import PaintTools from "./draw-screen/PaintTools.component";
 import RankingBoard from "./ranking-board/RankingBoard.component";
+import Canvas from "./draw-screen/Canvas.component";
+import BoxChatAnswer from "./chat-answer/BoxChatAnswer.component";
+import PaintTools from "./draw-screen/PaintTools.component";
 // Types
 import {
-  PaintContextType,
-  PenStyleType,
-  Point,
   RGBAColorType,
+  PaintContextType,
+  Point,
+  PenStyleType,
 } from "./draw-screen/draw";
 // Funtions
-import IntervalCanvas, { START_GAME } from "@/shared/components/IntervalCanvas";
-import Logo from "@/shared/components/Logo";
-import useToaster from "@/shared/hooks/useToaster";
+import { resetCanvas } from "./draw-screen/draw.helper";
 import { rgbaToHex } from "@/shared/lib/colors";
+import ActionButtons from "../../shared/components/ActionButtons";
+import Logo from "@/shared/components/Logo";
 import roomService from "@/shared/services/roomService";
 import { useParams } from "react-router-dom";
-import ActionButtons from "../../shared/components/ActionButtons";
-import { resetCanvas } from "./draw-screen/draw.helper";
 import { PEN_STYLE_BRUSH } from "./shared/constants/penStyles";
+import useToaster from "@/shared/hooks/useToaster";
+import IntervalCanvas, { START_GAME } from "@/shared/components/IntervalCanvas";
 
 
 export const PaintContext = createContext<PaintContextType | null>(null);
