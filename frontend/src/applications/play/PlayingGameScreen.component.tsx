eba7d9908aa14c6f7ffad1160bd088f578c40874
unknown <truongvanhao159@gmail.com>
2023-07-31 14:48:27 +0700

    fix host out -> start game again

--- frontend/src/applications/play/PlayingGameScreen.component.tsx
+++ frontend/src/applications/play/PlayingGameScreen.component.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import useDisableBackButton from '@/shared/hooks/useDisableBackButton';
-import { createContext, useCallback, useEffect, useRef, useState } from 'react';
+import { createContext, useEffect, useRef, useState } from 'react';
 // Variables
 import { DEFAULT_BLACK } from './shared/constants/color';
 // Components
@@ -42,7 +42,7 @@ import { RoomStatusType, RoomType } from '@/shared/types/room';
 import { useParams } from 'react-router-dom';
 import ActionButtons from '../../shared/components/ActionButtons';
 import { resetCanvas } from './draw-screen/draw.helper';
-import { DRAWER_CLEAR_CANVAS, NEW_PLAYER } from './shared/constants/drawEvent';
+import { NEW_PLAYER } from './shared/constants/drawEvent';
 import { PEN_STYLE_BRUSH } from './shared/constants/penStyles';
 
 export const PaintContext = createContext<PaintContextType | null>(null);
@@ -180,7 +180,7 @@ export default function PlayingGameScreen() {
 
   const isInterval = gameStatus !== PLAY_GAME;
 
-  const handleProgressTimeout = useCallback(() => {
+  const handleProgressTimeout = () => {
     if (!isHost || !socket || !codeRoom) return;
     if (gameStatus === PLAY_GAME) {
       socket?.emit(INTERVAL_SHOW_WORD, codeRoom);
@@ -191,7 +191,9 @@ export default function PlayingGameScreen() {
       return;
     }
 
+    
     if (gameStatus === INTERVAL_NEW_TURN) {
+      
       socket.emit(PLAY_GAME, codeRoom);
       socket.emit(GAME_PROGRESS, {
         codeRoom,
@@ -207,7 +209,7 @@ export default function PlayingGameScreen() {
       });
       return;
     }
-  }, [gameStatus]);
+  }
 
   return (
     <PaintContext.Provider
