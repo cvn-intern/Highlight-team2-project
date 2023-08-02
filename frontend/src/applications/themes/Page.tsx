2d2000d312464828839776921b83d72441b17ea6
hnvkhanh <hnvk101@gmail.com>
2023-08-02 14:04:40 +0700

    PBI20: add font

--- frontend/src/applications/themes/Page.tsx
+++ frontend/src/applications/themes/Page.tsx
@@ -5,27 +5,31 @@ import SloganImg from "@/shared/assets/slogan.png";
 import MainLayout from "@/shared/components/MainLayout";
 
 const CreateThemePage = () => {
-   
+
     return (
         <MainLayout>
             <div className="flex flex-col items-center justify-center w-full">
                 <Logo customClassname="max-md:mt-12" />
-                <img
-                    src={SloganImg}
-                    alt="Slogan"
-                    className="slogan-width slogan-responsive w-[250px] 2xl:w-[300px] mt-2.5 2xl:mt-5"
-                />
                 <div className="relative bg-white flex flex-col items-center mb-5 w-[92%] xl:w-3/4 2xl:w-3/5 min-h-[70vh] mt-5 rounded-2xl pb-5">
-                    <button
-                        onClick={()=>{}}
-                        className="left-1 mx-5 md:mr-10 absolute top-8 md:left-10"
-                    >
-                        <Triangle
-                            size={40}
-                            strokeWidth={2.5}
-                            className="-rotate-90 fill-[#f7b733] hover:opacity-80"
-                        />
-                    </button>                                       
+                    <div className="flex justify-between lg:w-full w-[70%] p-4">
+                        <div>
+                            <button
+                                onClick={()=>{}}
+                            >
+                                <Triangle
+                                    size={40}
+                                    strokeWidth={2.5}
+                                    className="-rotate-90 fill-[#f7b733] hover:opacity-80"
+                                />
+                            </button>
+                        </div>
+                        <div className="w-full max-lg:mt-2 text-center lg:mr-10">
+                            <p className="lg:text-4xl text-2xl font-balsamiq text-sky-600">
+                                THEME LAB
+                            </p>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </MainLayout >
