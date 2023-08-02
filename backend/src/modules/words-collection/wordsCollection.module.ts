e4d16f24c3d1db3e287f261b0310d6029fd908ee
trungnguyenhuynhminh46 <58035150+trungnguyenhuynhminh46@users.noreply.github.com>
2023-08-02 13:16:47 +0700

    Fix merge error

--- backend/src/modules/words-collection/wordsCollection.module.ts
+++ backend/src/modules/words-collection/wordsCollection.module.ts
@@ -10,22 +10,13 @@ import { WordModule } from '../word/word.module';
 import { WordService } from '../word/word.service';
 import { WordRepository } from '../word/word.repository';
 import { Word } from '../word/word.entity';
+import { RedisService } from '../redis/redis.service';
+import { RedisModule } from '../redis/redis.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([WordsCollection, Word]),
-    JwtModule,
-    UserModule,
-    WordModule,
-  ],
+  imports: [TypeOrmModule.forFeature([WordsCollection, Word]), JwtModule, UserModule, WordModule, RedisModule],
   controllers: [WordsCollectionController],
-  providers: [
-    WordsCollectionService,
-    Logger,
-    WordsCollectionRepository,
-    WordRepository,
-    WordService,
-  ],
+  providers: [WordsCollectionService, Logger, WordsCollectionRepository, WordRepository, WordService],
   exports: [WordsCollectionService],
 })
 export class WordsCollectionModule {}
