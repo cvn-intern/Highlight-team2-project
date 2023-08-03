8c3df5ff77355f68f04bad4d278add1885d8eda4
unknown <truongvanhao159@gmail.com>
2023-08-03 10:29:23 +0700

    fix chat in waiting for players

--- backend/src/modules/socket/gateways/chat.gateway.ts
+++ backend/src/modules/socket/gateways/chat.gateway.ts
@@ -20,26 +20,27 @@ export class ChatGateway extends SocketGateway {
     try {
       const roomId = extractIdRoom(msgBody.codeRoom);
       const round: RoomRound = await this.roomRoundService.getRoundOfRoom(roomId);
-      const answerRound: string = round.word;
-      const typeAnswer = checkTypeAnswer(answerRound.toLocaleUpperCase(), msgBody.message.toLocaleUpperCase());
-
+      const ROOM_CHAT = `${msgBody.codeRoom}-chat`;
+      
       const messageContent: Chat = {
         user: client.user.nickname,
         type: CHAT_ROOM_TYPE,
         message: msgBody.message,
       };
 
-      const ROOM_CHAT = `${msgBody.codeRoom}-chat`;
+      if(!round) {
+        return this.server.in(msgBody.codeRoom).emit(ROOM_CHAT, messageContent);
+      }
+      const answerRound: string = round.word;
+      const typeAnswer = checkTypeAnswer(answerRound.toLocaleUpperCase(), msgBody.message.toLocaleUpperCase());
 
       if (typeAnswer === ANSWER_CORRETLY || typeAnswer === ANSWER_APPROXIMATELY) {
         messageContent.message = SERVER_BLOCKED_MESSAGE_CONTENT;
         messageContent.type = BLOCK_MESSAGE;
         messageContent.user = '';
-        this.server.to(client.id).emit(ROOM_CHAT, messageContent);
-        return;
+        
+        return this.server.to(client.id).emit(ROOM_CHAT, messageContent);
       }
-
-      this.server.in(msgBody.codeRoom).emit(ROOM_CHAT, messageContent);
     } catch (error) {
       this.logger.error(error);
     }
